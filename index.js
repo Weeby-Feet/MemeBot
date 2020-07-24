1ed078eec824ae7ed30944b654539f2635adaf79
Nathan Ellison <nathan.ipadman@gmail.com>
2020-07-24 21:17:03 +1200

    Removing accidental additions

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ function caption_meme(message) {
   var formData = {
     template_id : args[1],
     username : "Weeby-Feet",
-    password : "bottyboy",
+    password : "PASSWORD",
     text0 : args[2],
     text1 : args[3],
     boxes : boxArr
